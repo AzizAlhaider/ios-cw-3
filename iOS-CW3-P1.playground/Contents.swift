9ae871f4a9928316f01fe35d13b4a995e560019e
AzizAlhaider <azizalhaider03@gmail.com>
2020-09-16 20:30:28 +0300

    Done

---
+++ iOS-CW3-P1.playground/Contents.swift
@@ -0,0 +1,6 @@
+func printMe(){
+    print("My name is Abdulaziz")
+    print("I'm 17 years old")
+    print("My grade is 87.45")
+}
+printMe()
---
+++ iOS-CW3-P2.playground/Contents.swift
@@ -0,0 +1,8 @@
+var number1 = 5
+var number2 = 6
+
+func add(number1: Int, number2: Int) -> Int{
+    return (number1 + number2)
+}
+var functionCall = add(number1: number1, number2: number2)
+print(functionCall)
